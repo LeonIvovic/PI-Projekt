1a55dba65be6f2e5b81e3c2cfe772a267c853311
MatAci <acingermateo@gmail.com>
2023-03-28 16:11:24 +0200

    Comments added
    Dodani komentari i napravljeno testiranje

--- Assets/Scripts/EndDetectionObjectDestroyer.cs
+++ Assets/Scripts/EndDetectionObjectDestroyer.cs
@@ -7,6 +7,7 @@ public class EndDetectionObjectDestroyer : MonoBehaviour
     // Start is called before the first frame update
    private void OnTriggerEnter2D(Collider2D collision)
     {
+        //kada dode do sudara PlatformDetectera i GroundSpawnera uništi PlatformDetecter
         if (collision.gameObject.CompareTag("GroundSpawner"))
         {
             Destroy(gameObject);
