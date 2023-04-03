ae85ebb003adacad9c2a70c78b79e9cf8418d0c7
LeonIvovic <104684302+LeonIvovic@users.noreply.github.com>
2023-04-04 01:35:52 +0200

    Platforms and spikes

--- Assets/Scripts/LevelElement.cs
+++ Assets/Scripts/LevelElement.cs
@@ -7,6 +7,10 @@ public class LevelElement : MonoBehaviour
     public GameObject element;
     private float length = 0;
 
+    public GameObject objectOnTop;
+    public int minObjectsOnTop = 0;
+    public int maxObjectsOnTop = 0;
+
     public float GetLength()
     {
         if (length != 0)
@@ -36,5 +40,21 @@ public class LevelElement : MonoBehaviour
     public void Generate(Vector2 position, Quaternion rotation, Transform parent)
     {
         Instantiate(element, position, rotation, parent);
+
+        if (objectOnTop != null && maxObjectsOnTop > 0)
+        {
+            int randomElementsCount = Random.Range(minObjectsOnTop, maxObjectsOnTop + 1);
+            float offset = length / randomElementsCount;
+            for (int i = 0; i < randomElementsCount; i++)
+            {
+                Instantiate
+                (
+                    objectOnTop,
+                    new Vector2(position.x - length / 2 + offset / 2 + i * offset, position.y + transform.localScale.y / 2),
+                    Quaternion.identity,
+                    parent
+                 );
+            }
+        }
     }
 }
