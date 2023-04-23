8ae2a1b0d538d28dad94221025156ea867f733de
MatAci <acingermateo@gmail.com>
2023-04-23 14:01:11 +0200

    Dodan JumpPad
    Nekakav ground na koji igrač skoci i on ga lansira visoko u zrak.

---
+++ Assets/Scripts/JumpPad.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float bounce = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision){
+
+        if(collision.gameObject.CompareTag("Player")){
+
+collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounce, ForceMode2D.Impulse);
+        }
+
+        
+    }
+}
